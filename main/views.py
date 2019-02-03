7a397e79e948c277a3a6961087163d2a409f2eba
Ivan <ivan@programmer.net>
2019-02-03 14:12:41 +1300

    minor design fixes

--- main/views.py
+++ main/views.py
@@ -10,7 +10,7 @@ from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.views.decorators.cache import cache_page
 
-
+# TimeToLive of redis cache
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
